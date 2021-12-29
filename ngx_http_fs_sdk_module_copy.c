aa3014117be78b0535fef263fff419e6810e1b56
Chadiii <chadilaoulaou@gmail.com>
2021-12-29 15:24:45 +0100

    fixing static problem

--- ngx_http_fs_sdk_module_copy.c
+++ ngx_http_fs_sdk_module_copy.c
@@ -28,17 +28,17 @@ static ngx_int_t ngx_http_fs_sdk_get_all_flags_handler(ngx_http_request_t *r);
 static char *ngx_http_add_params(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
 
+static char *ngx_http_fs_sdk(ngx_conf_t *cf, void *post, void *data);
+
+static ngx_conf_post_handler_pt ngx_http_fs_sdk_p = ngx_http_fs_sdk;
+
 typedef struct
 {
     ngx_array_t *params;
+    ngx_str_t visitor_id;
     
 } ngx_http_fs_sdk_init_loc_conf_t;
 
-typedef struct
-{
-    ngx_array_t *paramsGetFlags;
-
-} ngx_http_fs_sdk_get_all_flags_loc_conf_t;
 
 
 static void *
@@ -67,8 +67,6 @@ static char *context = NULL; */
 static ngx_str_t env_id_string;
 static ngx_str_t api_key_string;
 static ngx_str_t timeout_string;
-static ngx_str_t visitor_id_string;
-static ngx_str_t context_string;
 
 static void (*init_flagship)(char *, char *, int, char *);
 static char *(*get_all_flags)(char *, char *);
@@ -88,12 +86,20 @@ static ngx_command_t ngx_http_fs_sdk_commands[] = {
      offsetof(ngx_http_fs_sdk_init_loc_conf_t, params), /* No offset when storing the module configuration on struct. */
      NULL},
 
+     {ngx_string("set_visitor_id"),                                /* directive */
+     NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,                   /* location context and takes
+                                            no arguments*/
+     ngx_conf_set_str_slot,                             /* configuration setup function */
+     NGX_HTTP_LOC_CONF_OFFSET,                             /* No offset. Only one context is supported. */
+     offsetof(ngx_http_fs_sdk_init_loc_conf_t, visitor_id), /* No offset when storing the module configuration on struct. */
+     &ngx_http_fs_sdk_p},
+
      {ngx_string("get_all_flags"),                                /* directive */
-     NGX_HTTP_LOC_CONF | NGX_CONF_TAKE2,                   /* location context and takes
+     NGX_HTTP_LOC_CONF | NGX_CONF_NOARGS,                   /* location context and takes
                                             no arguments*/
      ngx_http_get_all_flags,                             /* configuration setup function */
      NGX_HTTP_LOC_CONF_OFFSET,                             /* No offset. Only one context is supported. */
-     offsetof(ngx_http_fs_sdk_get_all_flags_loc_conf_t, paramsGetFlags), /* No offset when storing the module configuration on struct. */
+     0, /* No offset when storing the module configuration on struct. */
      NULL},
 
     ngx_null_command /* command termination */
@@ -238,6 +244,9 @@ static ngx_int_t ngx_http_fs_sdk_get_all_flags_handler(ngx_http_request_t *r)
     ngx_chain_t out;
     u_char *message;
 
+    ngx_http_fs_sdk_init_loc_conf_t *cglcf;
+    cglcf = ngx_http_get_module_loc_conf(r, ngx_http_fs_sdk_module);
+
     /* Set the Content-Type header. */
     r->headers_out.content_type.len = sizeof("text/plain") - 1;
     r->headers_out.content_type.data = (u_char *)"text/plain";
@@ -264,7 +273,7 @@ static ngx_int_t ngx_http_fs_sdk_get_all_flags_handler(ngx_http_request_t *r)
 
     printf("after calling initilize flagship !");
 
-    flags = get_all_flags((char *)visitor_id_string.data, (char *)context_string.data);
+    flags = get_all_flags((char *)cglcf->visitor_id.data, "_");
     if (flags)
     {
         ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "getting VIP");
@@ -374,30 +383,15 @@ static char *ngx_http_get_all_flags(ngx_conf_t *cf, ngx_command_t *cmd, void *co
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
     clcf->handler = ngx_http_fs_sdk_get_all_flags_handler;
 
-    ngx_str_t *value_params;
-    ngx_array_t **paramsGetFlags;
-
-    value_params = cf->args->elts;
-
-    paramsGetFlags = (ngx_array_t **) ((char *) clcf + cmd->offset);
-
-    if(*paramsGetFlags == NULL){
-        return NGX_CONF_ERROR;
-    }
-
-    if (value_params[1].len == 0) {
-        return NGX_CONF_ERROR;
-    }
-
-    if (value_params[2].len == 0) {
-        return NGX_CONF_ERROR;
-    }
-    
-    visitor_id_string.data = value_params[1].data;
-    visitor_id_string.len = ngx_strlen(visitor_id_string.data);
-
-    context_string.data = value_params[2].data;
-    context_string.len = ngx_strlen(context_string.data);
-
     return NGX_CONF_OK;
 } /* ngx_http_get_all_flags */
+
+static char *ngx_http_fs_sdk(ngx_conf_t *cf, void *post, void *data){
+    
+    ngx_http_core_loc_conf_t *clcf;
+
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_http_fs_sdk_get_all_flags_handler;
+
+    return NGX_CONF_OK;
+}
