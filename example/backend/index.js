4edba5f438cb97cde3ec90eb8d10916fd5388cec
chadiii <blackbeard@DESKTOP-E1T0NV5.localdomain>
2022-03-16 18:20:16 +0100

    adding flagship sdk and updating without_module example

--- example/backend/index.js
+++ example/backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8081
+const { Flagship } = require("@flagship.io/js-sdk");
 
 app.set('etag', false)
 
@@ -14,11 +15,31 @@ app.get('/with_module', (req, res) => {
 })
 
 app.get('/without_module', (req, res) => {
-    if (req.get('user-agent').includes("Chrome")) {
-        res.send("Chrome")
-    } else {
-        res.send("Firefox")
-    }
+
+    Flagship.start("c0n48jn5thv01k0ijmo0", "BsIK86oh7c12c9G7ce4Wm1yBlWeaMf3t1S0xyYzI");
+
+    const visitor_id = req.get('x-visitor-id')
+    const visitor_context = req.get('x-visitor-context')
+    const [visitorContextKey, visitorContextValue] = visitor_context.split(':');
+
+    const visitor = Flagship.newVisitor({
+        visitorId: visitor_id,
+        context: { [visitorContextKey]: visitorContextValue }
+    });
+
+    visitor.on("ready", (error) => {
+        if (error) {
+            res.status(500).send("Internal Error (Visitor)")
+            return;
+        }
+        const flagKey = visitor.getFlag("IsVIP", false)._key;
+        const flagValue = visitor.getFlag("IsVIP", false).getValue();
+        res.status(200).send(
+            `   <p>Visitor id : ${visitor_id}</p>
+                <p>Visitor context : ${visitor_context}</p>
+                <p>Flags : ${flagKey}: ${flagValue}</p>
+            `)
+    });
 })
 
 app.listen(port, () => {
