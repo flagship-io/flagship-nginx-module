a59fe1124b4137af7ceb8efa63c73a405b2fbefd
chadiii <blackbeard@DESKTOP-E1T0NV5.localdomain>
2022-03-25 18:29:18 +0100

    refactoring the projet

---
+++ example/standalone/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker build -f Dockerfile.standalone --build-arg NGINX_VERSION=1.21.6 -t flagshipio/nginx:nginx-1.21.6 .
+docker run --rm -it -p 8080:80 -t --name nginx-standalone \
+ -v $(pwd)/example/standalone/config/nginx-conf:/etc/nginx/sites-available/nginx-conf.example \
+ -v $(pwd)/example/standalone/config/fs_module.conf:/etc/nginx/modules-enabled/fs.conf \
+ flagshipio/nginx:nginx-1.21.6
